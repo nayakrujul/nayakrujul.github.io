13daf5b80b1292806245bb0bcbf1a63467b2fb03
nayakrujul <nayakrujul@hotmail.com>
2024-01-30 18:33:40 +0000

    Summary questions

--- CompSci/spoilers.js
+++ CompSci/spoilers.js
@@ -1,6 +1,9 @@
 [...document.querySelectorAll(".spoiler")].map(p => {
     p.onclick = (event) => {
-        let elem = event.srcElement.parentElement;
+        let elem = event.srcElement;
+        while (![...elem.classList].includes("spoiler")) {
+            elem = elem.parentElement;
+        }
         if ([...elem.classList].includes("show-spoiler")) {
             elem.classList.remove("show-spoiler");
         } else {
